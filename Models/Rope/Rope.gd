cd1c7b49c4a1c130153c610c3a201b2969f41f8f
Tenes <enzo.bruni@aol.fr>
2024-03-01 23:11:44 +0100

    Adds rope logic

---
+++ Models/Rope/Rope.gd
@@ -0,0 +1,10 @@
+extends Line2D
+class_name Rope
+
+var linkedHarpoon : Harpoon;
+
+func linkToHarpoon(harpoon: Harpoon) -> void:
+	width = 2;
+	add_point(Vector2(0, 0));
+	add_point(harpoon.getRopeHolePoint());
+	linkedHarpoon = harpoon;
--- Scripts/city_scene.gd
+++ Scripts/city_scene.gd
@@ -1,14 +1,11 @@
 extends Node2D
 
 
-@onready var Rope : Line2D = $Rope
+@onready var Rope : Rope = $Rope
 @onready var Harpoon : Harpoon = $Harpoon
 
 func _ready() -> void:
-	Rope.width = 2;
-	Rope.add_point(Vector2(0, 0));
-	Rope.add_point(Harpoon.getRopeHolePoint());
-
+	Rope.linkToHarpoon(Harpoon);
 
 func _process(delta: float) -> void:
 	pass
